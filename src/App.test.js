de448767978165d78fb2026056b7758ac0e2ee88
Curtis Timson <curt@live.co.uk>
2018-07-12 15:26:02 +0100

    Remove default tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,12 +5,6 @@ import App from './App';
 
 describe('App', () => {
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-
   it('matches snapshot', () => {
     const tree = renderer
       .create(<App/>);
--- src/Child.test.js
+++ src/Child.test.js
@@ -5,12 +5,6 @@ import Child from './Child';
 
 describe('Child', () => {
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Child />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-
   it('matches snapshot', () => {
     const tree = renderer
       .create(<Child/>);
