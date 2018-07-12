d49bf8c10fee30a481029d2fb7b8603c04fa845a
Curtis Timson <curt@live.co.uk>
2018-07-12 15:06:06 +0100

    Add snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('matches snapshot', () => {
+    const tree = renderer
+      .create(<App/>);
+    expect(tree).toMatchSnapshot();
+  });
+
 });
