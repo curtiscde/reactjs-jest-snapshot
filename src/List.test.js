185090df6ed8ae55f1ee7c832a707044ba23b3cd
Curtis Timson <curt@live.co.uk>
2018-07-12 20:30:36 +0100

    Add list snapshot when items are passed in

--- src/List.test.js
+++ src/List.test.js
@@ -4,9 +4,16 @@ import List from './List';
 
 describe('List', () => {
 
-  it('matches snapshot', () => {
+  it('matches snapshot when empty list', () => {
     const tree = renderer
       .create(<List/>);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('matches snapshot when items passed in', () => {
+    const items = ['abc', 'def', 'ghi'];
+    const tree = renderer
+      .create(<List items={items}/>);
     expect(tree).toMatchSnapshot();
   });
 
